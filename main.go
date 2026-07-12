0a23309559200be5dd7e0862275e1778b8819863
Uberth Hernandez <ing.uberthhernandez@gmail.com>
2026-07-12 18:08:57 -0400

    added http server

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
+
+// handle home route "/"
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from  Snippetbox app! 🚀"))
+}
 
 func main() {
-	fmt.Println("Hello there!")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+
+	PORT := "4000"
+
+	log.Print("Server running on localhost:" + PORT)
+	err := http.ListenAndServe(":4000", mux)
+	// Other way to run the server ↓↓
+	// log.Fatal(http.ListenAndServe(":"+PORT, mux))
+	log.Fatal(err)
 }
