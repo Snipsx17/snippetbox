6092bdbea3751da621d328ec1910c1dad83ad1e5
Uberth Hernandez <ing.uberthhernandez@gmail.com>
2026-07-14 20:30:45 -0400

    added handler

--- main.go
+++ main.go
@@ -1,18 +1,52 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
+type SnippetHandler struct {
+}
+
+func NewSnippetHandler() *SnippetHandler {
+	return &SnippetHandler{}
+}
+
 // handle home route "/"
-func home(w http.ResponseWriter, r *http.Request) {
+func (h *SnippetHandler) Home(w http.ResponseWriter, r *http.Request) {
+	log.Printf("[%s] %s", r.Method, r.URL.Path)
 	w.Write([]byte("Hello from  Snippetbox app! 🚀"))
 }
 
+func (h *SnippetHandler) ViewSnippet(w http.ResponseWriter, r *http.Request) {
+	log.Printf("[%s] %s", r.Method, r.URL.Path)
+
+	id, err := strconv.Atoi(r.PathValue("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	response := fmt.Sprintf("View snippet with ID: %d", id)
+	w.Write([]byte(response))
+}
+
+func (h *SnippetHandler) CreateSnippet(w http.ResponseWriter, r *http.Request) {
+	log.Printf("[%s] %s", r.Method, r.URL.Path)
+	w.Write([]byte("Create snippet"))
+}
+
+var Handler = NewSnippetHandler()
+
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
+
+	//routes
+	mux.HandleFunc("/{$}", Handler.Home) // {$} restrict this route to strict matches on / only
+	mux.HandleFunc("/snippet/view/{id}", Handler.ViewSnippet)
+	mux.HandleFunc("/snippet/create", Handler.CreateSnippet)
 
 	PORT := "4000"
 
